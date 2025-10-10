b16cda216bb45f6b3b2d4fc8da3a151d6b5d4d66
darshdesai00 <darshdesai50@icloud.com>
2025-10-09 18:01:35 -0700

    Final version – Chair array auto-populated with random data from default constructor

--- main.cpp
+++ main.cpp
@@ -23,8 +23,7 @@ public:
         }
     }
 
-    // new parameter constructor (legs + price array)
-    Chair(int l, double arr[]) {
+    Chair(int l, double arr[]) {  // legs + price array
         legs = l;
         prices = new double[SIZE];
         for (int i = 0; i < SIZE; i++)
@@ -59,25 +58,22 @@ int main() {
     srand(time(0));  // seed random numbers
     cout << fixed << setprecision(2);
 
-    Chair *chairPtr = new Chair;  // uses random constructor
+    Chair *chairPtr = new Chair;  // random constructor
     chairPtr->print();
 
-    // now using the new parameter constructor
     double priceArr[3] = {199.99, 299.99, 399.99};
-    Chair *livingChair = new Chair(3, priceArr);
+    Chair *livingChair = new Chair(3, priceArr);  // parameter constructor
     livingChair->print();
     delete livingChair;
     livingChair = nullptr;
 
+    // array ow uses only default constructor
     Chair *collection = new Chair[SIZE];
-    collection[0].setLegs(4);
-    collection[0].setPrices(441.41, 552.52, 663.63);
-    collection[1].setLegs(4);
-    collection[1].setPrices(484.84, 959.59, 868.68);
-    collection[2].setLegs(4);
-    collection[2].setPrices(626.26, 515.15, 757.57);
     for (int i = 0; i < SIZE; i++)
         collection[i].print();
-    
+
+    delete[] collection;
+    delete chairPtr;
+
     return 0;
 }
